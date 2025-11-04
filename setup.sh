872cd6a34255e5f2a7a20ed2ac48f1f7f55a9b9f
MethaRahmawati <145530463+MethaRahmawati@users.noreply.github.com>
2025-11-04 19:55:05 +0700

    fix: remove windows-only package

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 composer install --no-interaction --prefer-dist --optimize-autoloader
+npm ci --omit=optional
+npm run build
 php artisan key:generate
 php artisan migrate --force
 php artisan storage:link
