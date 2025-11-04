a6f8477b31c46019f83fc018e853073c0e8ef048
MethaRahmawati <145530463+MethaRahmawati@users.noreply.github.com>
2025-11-04 22:06:01 +0700

    fix: TrustProxies constant for Railway

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -2,8 +2,8 @@
 
 namespace App\Http\Middleware;
 
+use Illuminate\Http\Middleware\TrustProxies as Middleware;
 use Illuminate\Http\Request;
-use Fideloper\Proxy\TrustProxies as Middleware;
 
 class TrustProxies extends Middleware
 {
@@ -12,15 +12,12 @@ class TrustProxies extends Middleware
      *
      * @var array|string|null
      */
-    protected $proxies = '*';
+    protected $proxies = '*'; // izinkan semua proxy (penting untuk Railway)
 
     /**
      * The headers that should be used to detect proxies.
      *
      * @var int
      */
-    protected $headers = Request::HEADER_X_FORWARDED_FOR | 
-                         Request::HEADER_X_FORWARDED_HOST | 
-                         Request::HEADER_X_FORWARDED_PORT | 
-                         Request::HEADER_X_FORWARDED_PROTO;
+    protected $headers = Request::HEADER_X_FORWARDED_ALL;
 }
